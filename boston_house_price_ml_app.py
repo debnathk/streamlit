e2ed6f55c575697c6b926146879bcc08ea6d25ba
Kusal <debnathk1997@gmail.com>
2022-06-05 03:14:31 +0530

    Final commit with prediction and feature importance plots

--- boston_house_price_ml_app.py
+++ boston_house_price_ml_app.py
@@ -26,7 +26,8 @@ st.sidebar.header('Specify Input Parameters')
 def user_input_features():
     CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
     ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
+    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(),
+                              X.INDUS.max(), X.INDUS.mean())
     CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
     NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
     RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
@@ -34,9 +35,11 @@ def user_input_features():
     DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
     RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
     TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
+    PTRATIO = st.sidebar.slider(
+        'PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
     B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
+    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(),
+                              X.LSTAT.max(), X.LSTAT.mean())
     data = {'CRIM': CRIM,
             'ZN': ZN,
             'INDUS': INDUS,
@@ -52,3 +55,41 @@ def user_input_features():
             'LSTAT': LSTAT}
     features = pd.DataFrame(data, index=[0])
     return features
+
+
+df = user_input_features()
+
+# Main Panel
+
+# Print specified input parameters
+st.header('Specified Input Parameters')
+st.write(df)
+st.write('---')
+
+# Build Regression Model
+model = RandomForestRegressor()
+model.fit(X, y)
+
+# Apply Model to make prediction
+prediction = model.predict(df)
+
+st.header('Prediction of MEDV')
+st.write(prediction)
+st.write('---')
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st.header('Feature Importance')
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
+st.write('---')
+
+plt.title('Feature importance based on SHAP values (Bar)')
+shap.summary_plot(shap_values, X, plot_type="bar")
+st.pyplot(bbox_inches='tight')
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
