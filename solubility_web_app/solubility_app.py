721e9a84e4952229431dea208166a92f70d53a21
Kusal <debnathk1997@gmail.com>
2022-06-06 17:33:41 +0530

    Add app.py file

---
+++ solubility_web_app/solubility_app.py
@@ -0,0 +1,8 @@
+# Importing libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
+from PIL import Image
+from rdkit import Chem
+from rdkit.Chem import Descriptors
